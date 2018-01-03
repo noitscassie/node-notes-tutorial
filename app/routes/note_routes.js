52f28b16ad21cf349ba8d78f15aff972f32bc3e8
Peter Johnstone <peter.wdj@gmail.com>
2018-01-03 22:26:50 +0000

    Add requests to GET and DELETE notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,18 @@
 module.exports = function(app, db) {
+  const ObjectID = require('mongodb').ObjectID;
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error':'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
@@ -6,6 +20,18 @@ module.exports = function(app, db) {
         res.send({ 'error': 'An error has occurred' });
       } else {
         res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({ 'error':'An error has occurred' } );
+      } else {
+        res.send(`Note ${id} deleted!`);
       }
     });
   });
