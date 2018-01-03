cc907f9084b217fdd8409a81ae5f720f22b35fac
Peter Johnstone <peter.wdj@gmail.com>
2018-01-03 22:31:16 +0000

    Refactor repeated error string into const errorMessage

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,13 @@
 module.exports = function(app, db) {
   const ObjectID = require('mongodb').ObjectID;
+  const errorMessage = { 'error':'An error has occurred' };
 
   app.get('/notes/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
     db.collection('notes').findOne(details, (err, item) => {
       if (err) {
-        res.send({ 'error':'An error has occurred' });
+        res.send(errorMessage);
       } else {
         res.send(item);
       }
@@ -17,7 +18,7 @@ module.exports = function(app, db) {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
-        res.send({ 'error': 'An error has occurred' });
+        res.send(errorMessage);
       } else {
         res.send(result.ops[0]);
       }
@@ -29,7 +30,7 @@ module.exports = function(app, db) {
     const details = { '_id': new ObjectID(id) };
     db.collection('notes').remove(details, (err, item) => {
       if (err) {
-        res.send({ 'error':'An error has occurred' } );
+        res.send(errorMessage);
       } else {
         res.send(`Note ${id} deleted!`);
       }
