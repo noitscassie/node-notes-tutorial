4645b0fbe2fdcdabc97cb68f5cee4f9aafa33d56
Peter Johnstone <peter.wdj@gmail.com>
2018-01-03 22:42:19 +0000

    Add functionality to update notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -36,4 +36,17 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+        res.send(errorMessage);
+      } else {
+        res.send(note);
+      }
+    });
+  });
 };
